0b2e4af7d9cb2451046af262def2fb08a9ae5f49
Alex Silva <106414021+httplex@users.noreply.github.com>
2025-02-06 23:45:08 -0300

    Atualizar o TesteBancoDoBrasil.java

--- src/lista04/ex01_ex02/TesteBancoDoBrasil.java
+++ src/lista04/ex01_ex02/TesteBancoDoBrasil.java
@@ -3,7 +3,7 @@ package lista04.ex01_ex02;
 public class TesteBancoDoBrasil {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method
 
 	}
 
